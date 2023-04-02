dff711886ec49d424312e6a82201df0151a03ceb
Vagish Mohanakumar <iamvagish6@gmail.com>
2023-04-02 15:18:29 +0530

    creating a main class

--- arp_receiver.py
+++ arp_receiver.py
@@ -6,20 +6,30 @@ get_ip = input("[+] IP > ")
 get_mac = "ff:ff:ff:ff:ff:ff"
 
 
-def scan(ip, mac):
-    # creating
-    arp_request = scapy.ARP(pdst=ip)
-    print(arp_request)
-    arp_request.show()
+class ARP():
+    def __init__(self):
+        pass
 
-    # broadcast MAC address
-    broadcast = scapy.Ether(dst=mac)
-    print(broadcast)
-    broadcast.show()
+    def scan(self, ip, mac):
+        # creating
+        arp_request = scapy.ARP(pdst=ip)
+        print(arp_request)
+        arp_request.show()
 
-    arp_request_broadcast = broadcast/ arp_request
-    print(arp_request_broadcast)
-    arp_request_broadcast.show()
+        # broadcast MAC address
+        broadcast = scapy.Ether(dst=mac)
+        print(broadcast)
+        broadcast.show()
+
+        arp_request_broadcast = broadcast/ arp_request
+        print(arp_request_broadcast)
+        arp_request_broadcast.show()
 
 
-scan(get_ip, get_mac)
+def main():
+    arp_receiver = ARP()
+    arp_receiver.scan(get_ip, get_mac)
+
+
+if __name__ == "__main__" :
+    main()
