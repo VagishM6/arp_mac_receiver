cf4dc391b9b676f7362eef2ccfaa7aa2707291fe
Vagish Mohanakumar <iamvagish6@gmail.com>
2023-04-02 15:14:12 +0530

    adding functions to the script

--- arp_receiver.py
+++ arp_receiver.py
@@ -2,46 +2,6 @@ import subprocess
 import scapy.all as scapy
 import re
 
-
-# ip_range = int(input("[+] Enter range of IP to scan: "))
-#
-#
-# def arp_ping(ip):
-#     if ip > 24:
-#         print("[-] Range exceeded the ip limit")
-#     else:
-#         store_ip = scapy.arping(f"192.168.1.1/{ip}")
-#         return store_ip
-
-
-# arp_res = arp_ping(ip_range)
-# print("results: " + str(arp_res))
-
-
-
-
-
-# # arp packet generator
-# def scan(ip, mac):
-#     # scapy.arping(ip)
-#
-#     # creating an instance of - scapy ARP class
-#     arp_request = scapy.ARP(pdst=ip)
-#     # list options of scapy ARP class parameters
-#     # scapy.ls(arp_request)
-#     print(arp_request.summary())
-#
-#     # creating an instance of - scapy Ether class
-#     broadcast = scapy.Ether(dst=mac)
-#     # list options of scapy Ether class parameters
-#     # scapy.ls(broadcast)
-#     print(broadcast.summary())
-#
-#
-# # sending arp packets to the target ip
-# scan("192.168.8.1/24", "ff:ff:ff:ff:ff:ff")
-
-
 get_ip = input("[+] IP > ")
 get_mac = "ff:ff:ff:ff:ff:ff"
 
